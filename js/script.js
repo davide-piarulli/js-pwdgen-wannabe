54a175cd9a2c6cc07beb13f60de3812d87f41c16
davide-piarulli <davide.piarulli.1@gmail.com>
2024-02-19 13:37:12 +0000

    Milestone1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const name = prompt('Scrivi il tuo nome');
+const lastName = prompt('Scrivi il tuo cognome');
+
+const fullName = name + lastName;
+console.log(fullName);
+
+const favColor = prompt('Quale è il tuo colore preferito?');
+
+const pw = fullName + favColor;
+console.log(fullName + favColor + '24');
