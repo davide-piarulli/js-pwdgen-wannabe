6205f6a2c3265837dae22a9ae1f7ebea35698cbe
davide-piarulli <davide.piarulli.1@gmail.com>
2024-02-19 13:55:28 +0000

    Milestone2

--- js/script.js
+++ js/script.js
@@ -2,9 +2,17 @@ const name = prompt('Scrivi il tuo nome');
 const lastName = prompt('Scrivi il tuo cognome');
 
 const fullName = name + lastName;
-console.log(fullName);
 
 const favColor = prompt('Quale è il tuo colore preferito?');
 
-const pw = fullName + favColor;
-console.log(fullName + favColor + '24');
+const number = parseInt('24'); 
+
+const pw = fullName + favColor + number;
+console.log(pw);
+
+document.getElementById('password').innerHTML =
+`
+  <p>
+    La tua password è: <strong>${pw}</strong>
+  </p>
+`;
